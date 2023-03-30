3ee8ec75cb598f8048156ccf97552def82008b52
waripex <85075547+waripex@users.noreply.github.com>
2023-03-30 00:09:47 -0300

    Add files via upload
    main progress

--- ProyectoProgra/src/main/java/Proyectoto/Mainproyecto.java
+++ ProyectoProgra/src/main/java/Proyectoto/Mainproyecto.java
@@ -1,7 +1,17 @@
 package Proyectoto;
 
+import java.util.*;
+
+
 public class Mainproyecto {
     public static void Main(String[] args){
+       //Datos iniciales
+       Reunion r1 = new Reunion("almuerzo familiar",2,1,"almuerzo");
+       Reunion r2 = new Reunion("Junta jefe",3,1,"urgente");
+       Reunion r3 = new Reunion("birras con los k",5,1,"amigos");
+       
+       //Crear semanas
+       
         
     }
 }
