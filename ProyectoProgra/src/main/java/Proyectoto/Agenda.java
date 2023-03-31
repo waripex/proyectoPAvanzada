83fc494159caf5a8006afb70a5008acececb73bf
Cristofer-Contreras <111030437+Cristofer-Contreras@users.noreply.github.com>
2023-03-31 01:49:17 -0300

    Add files via upload

---
+++ ProyectoProgra/src/main/java/Proyectoto/Agenda.java
@@ -0,0 +1,31 @@
+package Proyectoto;
+import java.util.*;
+/**
+ *
+ * @author Cristofer
+ */
+public class Agenda {
+    private int idSemana;
+    private ArrayList<Reunion> actividades;
+    
+    public Agenda(int idSemana){
+        this.idSemana = idSemana;
+        actividades = new ArrayList();
+    }
+    public Agenda(int idSemana, ArrayList ra){
+        this.idSemana = idSemana;
+        actividades = new ArrayList();
+        actividades.addAll(ra);
+    }
+    
+    public int getIdSemana(){
+        return idSemana;
+    }
+    public void setIdSemana(int idSemana){
+        this.idSemana = idSemana;
+    }
+    public void agregaractividad(Reunion rr){
+        //validar no estar repetido
+        actividades.add(rr);
+    }
+}
--- ProyectoProgra/src/main/java/Proyectoto/Mainproyecto.java
+++ ProyectoProgra/src/main/java/Proyectoto/Mainproyecto.java
@@ -1,17 +1,19 @@
 package Proyectoto;
 
 import java.util.*;
-
-
 public class Mainproyecto {
     public static void Main(String[] args){
-       //Datos iniciales
-       Reunion r1 = new Reunion("almuerzo familiar",2,1,"almuerzo");
-       Reunion r2 = new Reunion("Junta jefe",3,1,"urgente");
-       Reunion r3 = new Reunion("birras con los k",5,1,"amigos");
-       
-       //Crear semanas
-       
+        Reunion d1 = new Reunion("cita medico",1,1,"salud");
         
+        
+        Agenda sem1 = new Agenda(1);
+        
+        Map<Integer,Agenda> rr = new HashMap();
+        rr.put(1,sem1);
+        
+        Agenda aa = rr.get(1);
+        aa.agregaractividad(d1);
+        
+        System.out.print(rr.get(1));
     }
 }
