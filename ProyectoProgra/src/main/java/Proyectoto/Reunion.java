6be19d332436591f312db0009a51d2f2734f3f54
Cristofer-Contreras <111030437+Cristofer-Contreras@users.noreply.github.com>
2023-04-01 20:51:29 -0300

    Add files via upload

--- ProyectoProgra/src/main/java/Proyectoto/Reunion.java
+++ ProyectoProgra/src/main/java/Proyectoto/Reunion.java
@@ -1,42 +1,13 @@
-package Proyectoto;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto;
 
+/**
+ *
+ * @author Cristofer
+ */
 public class Reunion {
-    private String nombre;
-    private int dia;
-    private int semana;
-    private String etiqueta;// mapa 
     
-    public Reunion (String nombre, int dia, int semana, String etiqueta){
-        this.nombre = nombre;
-        this.dia = dia;
-        this.semana = semana;
-        this.etiqueta = etiqueta;
-    }
-    // Setter &  getters 
-    public void setNom(String nombre){
-        this.nombre = nombre;
-    }
-    public void setDia(int dia){
-        this.dia = dia;
-    }
-    public void setEtiqueta(String etiqueta){
-        this.etiqueta = etiqueta;
-    }
-    public void setSemana(int semana){
-        this.semana = semana;
-    }
-    
-    
-    public String getNom(){
-        return nombre;
-    }
-    public int getDia(){
-        return dia;
-    }
-    public String getEtiqueta(){
-        return etiqueta;
-    }
-    public int getSemana(){
-        return semana;
-    }
 }
